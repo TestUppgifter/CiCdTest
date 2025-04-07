8d2d7bfde20f8060f84dd66e9a96748c1ea98469
TestUppgifter <k2376@pm.me>
2025-04-07 14:10:52 +0200

    add more tests

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,4 +5,16 @@ public class Calculator {
     public int calc(int a, int b){
         return a + b;
     }
+
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+    public double div(int a, int b){
+        return (double) a /b;
+    }
+
+    public double multi(int a, int b){
+        return (double) a * b;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,15 +5,30 @@ import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 public class CalculatorTest {
 
+    private final Calculator calculator = new Calculator();
+
     @Test
     void testCalc(){
-
-        final Calculator calculator = new Calculator();
-
         int result = calculator.calc(3,5);
-
         assertEquals("test calc", 8, result);
+    }
 
+    @Test
+    void testSub(){
+        int result = calculator.sub(5,3);
+        assertEquals("test sub", 2, result);
+    }
+
+    @Test
+    void testDiv(){
+        double result = calculator.div(6, 3);
+        assertEquals("test div", 2.0, result);
+    }
+
+    @Test
+    void testMulti(){
+        double result = calculator.multi(5, 3);
+        assertEquals("test multi", 15.0, result);
     }
 
 }
